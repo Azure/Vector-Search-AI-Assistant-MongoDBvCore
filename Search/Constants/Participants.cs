37a088d143df180e7efa5d3a44b3368c59dac2e5
Mark Brown <mjbrown@microsoft.com>
2023-10-19 12:21:05 -0700

    Initial Commit

---
+++ Search/Constants/Participants.cs
@@ -0,0 +1,7 @@
+namespace Search.Constants;
+
+public enum Participants
+{
+    User = 0,
+    Assistant
+}
---
+++ Vectorize/Options/OpenAi.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Vectorize.Options
+{
+    public record OpenAi
+    {
+        public string? Endpoint { get; set; }
+
+        public string? Key { get; set; }
+
+        public string? EmbeddingsDeployment { get; set; }
+
+        public string? MaxTokens { get; set; }
+
+        public ILogger? Logger { get; set; }
+    }
+}
